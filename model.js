90ff772b7565b357469078093c3c3228c5a25021
abhvit <47032085+abhvit@users.noreply.github.com>
2024-06-27 00:35:16 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+//csv schema design
+const csvSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//passing the csvSchema instance to mongoose.model
+const CSV = mongoose.model("CSV", csvSchema);
+
+//exporting the schema to be used further
+module.exports = CSV;
